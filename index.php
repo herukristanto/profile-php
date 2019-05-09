b9d64e1d6d91c7e1e1763b934ff6a160d31d034b
Heru Kristanto <herukristanto90@gmail.com>
2019-05-09 20:41:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Heru Kristanto - Portfolio</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <div class="navbar-fixed">  
+    <nav class="light-blue lighten-1" role="navigation">
+      <div class="nav-wrapper container">
+        <a id="logo-container" href="index.php" class="brand-logo">Heru Kristanto</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <ul id="nav-mobile" class="sidenav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
+  </div>
+
+<section id="home" class="home scrollspy">
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          <br><br>
+          <h1 class="header center orange-text">HERU KRISTANTO</h1>
+            <div class="row center">
+              <img class="circle  responsive-img  dude-pic "  src="images/profile1.jpg" height="200px" width="200px"  >
+              <br><br><br>
+            </div>
+        </div>
+      
+        <div class="row center">
+          <h5 class="header col s12 light">
+            Pengalaman saya yang beragam di bidang TI (misalnya pemrograman, arsitektur perangkat lunak, perancang pengalaman pengguna, pengembang web, dll) berasal dari keinginan sederhana untuk memberdayakan kehidupan manusia dengan teknologi komputasi. Dunia menjadi kecil dan semakin kecil dari hari ke hari. Batas geografis menghilang; Perbedaan budaya memudar. Umat manusia akan datang bersama. Itulah yang mendorong saya: membantu membuka jalan menuju masyarakat global yang diberdayakan.
+          </h5>
+        </div>
+      
+        <div class="row center">
+          <a href="" class="btn-large waves-effect waves-light orange">Get Started</a>
+        </div>
+        <br><br>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="portfolio" class="portfolio scrollspy">
+  <div class="container">
+    <div class="row">
+      <br><br><br>
+      <h3 class="header center light">Portfolio</h3>
+
+      <div class="col s3">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/gallery/thumb-1.jpg">
+            <span class="card-title">CRUD RoR</span>
+          </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+          <div class="card-action">
+            <a href="https://app-0033.herokuapp.com/">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s3">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/gallery/image-2.jpg">
+            <span class="card-title">SPK Hotel Admin</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="https://spk-hotel.herokuapp.com/">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s3">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/gallery/image-3.jpg">
+            <span class="card-title">SPK Hotel Client</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="https://spk-hotel.herokuapp.com/wisatawan/public/listdata.php">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s3">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/gallery/image-4.jpg">
+            <span class="card-title">SIAKAD</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="contact" class="contact scrollspy">
+  <div class="container">
+    <br><br><br>
+    <h3 class="light grey-text text-darken-3 center">Contact Us</h3>
+    <div class="row">
+      <div class="col m5 s12">
+        <div class="card-panel orange center white-text">
+          <i class="material-icons">email</i>
+          <h5>Contact</h5>
+          <p>lorem hdjdkk pahhh poooo  saya juga bisa menari sepert sperti anda yang sedang dudukoiiii</p>
+        </div>
+          <ul class="collection with-header">
+          <li class="collection-header"><h4>Alamat</h4></li>
+          <li class="collection-item">Profile Heru Kristanto</li>
+          <li class="collection-item">Jl.Swadaya, Pejaten Timur, Pasar Minggu</li>
+          <li class="collection-item">Jakarta Selatan, Indonesia</li>
+        </ul>
+      </div>
+      <div class="col m7 s12">
+        <form>
+          <div class="card-panel">
+            <h5>Please fill out this form</h5>
+            <div class="input-field">
+              <input type="text" name="name" id="name">
+              <label for="name">Name</label>
+            </div>
+            <div class="input-field">
+              <input type="email" name="email" id="email">
+              <label for="email">Email</label>
+            </div>
+            <div class="input-field">
+              <input type="number" name="phone" id="phone">
+              <label for="phone">Phone Number</label>
+            </div>
+            <div class="input-field">
+              <textarea name="massage" id="massage" class="materialize-textarea"></textarea>
+              <label for="massage">Massage</label>
+            </div>
+            <button type="submit" class="btn orange">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <br><br>
+</section>
+
+  <footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Biodata Diri</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <!-- <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div> -->
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Phone  : +62 813 1957 0033</a></li>
+            <li><a class="white-text" href="#!">Email  : herukristanto90@gmail.com</a></li>
+            <li><a class="white-text" href="https://github.com/herukristanto">Github : herukristanto</a></li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="">Heru Kristanto</a>
+      </div>
+    </div>
+
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <script>
+    const scroll = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(scroll, {
+      scrollOffset: 10
+    });
+
+  //   $(document).ready(function(){
+  //   $('.scrollspy').scrollSpy();
+  // });
+  </script>
+
+
+  </body>
+</html>
