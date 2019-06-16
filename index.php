11c1ca2254ce0257c7bbbe22fb4aeedd0d19254c
Heru kristanto <herukristanto90@gmail.com>
2019-06-17 02:15:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,4 @@
-<?php
-      $statusMsg = '';
-$msgClass = '';
-if(isset($_POST['submit'])){
-    // Get the submitted form data
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    
-    // Cek apakah ada data yang belum diisi
-    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
-        
-        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-            $statusMsg = 'Please enter your valid email.';
-            $msgClassk = 'errordiv';
-        }else{
-            // Pengaturan penerima email dan subjek email
-            $toEmail = 'herukristanto90@gmail.com'; // Ganti dengan alamat email yang Anda inginkan
-            $emailSubject = 'Pesan website dari '.$name;
-            $htmlContent = '<h2> via Form Kontak Website</h2>
-                <h4>Name</h4><p>'.$name.'</p>
-                <h4>Email</h4><p>'.$email.'</p>
-                <h4>Subject</h4><p>'.$subject.'</p>
-                <h4>Message</h4><p>'.$message.'</p>';
-            
-            // Mengatur Content-Type header untuk mengirim email dalam bentuk HTML
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            
-            // Header tambahan
-            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
-            
-            // Send email
-            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
-                $statusMsg = 'Pesan Anda sudah terkirim dengan sukses !';
-                $msgClass = 'succdiv';
-            }else{
-                $statusMsg = 'Maaf pesan Anda gagal terkirim, silahkan ulangi lagi.';
-                $msgClass = 'errordiv';
-            }
-        }
-    }else{
-        $statusMsg = 'Harap mengisi semua field data';
-        $msgClass = 'errordiv';
-    }
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -192,10 +145,8 @@ if(isset($_POST['submit'])){
         </ul>
       </div>
       <div class="col m7 s12">
-        <?php if(!empty($statusMsg)){ ?>
-            <p class="statusMsg <?php echo !empty($msgClass)?$msgClass:''; ?>"><?php echo $statusMsg; ?></p>
-        <?php } ?>
-        <form action="" method="post">
+        
+        <form action="kirim.php" method="post">
           <div class="card-panel">
             <h5>Please fill out this form</h5>
             <div class="input-field">
@@ -207,7 +158,7 @@ if(isset($_POST['submit'])){
               <label for="email">Email</label>
             </div>
             <div class="input-field">
-              <input type="number" name="subject" id="phone">
+              <input type="number" name="phone" id="phone">
               <label for="phone">Phone Number</label>
             </div>
             <div class="input-field">
