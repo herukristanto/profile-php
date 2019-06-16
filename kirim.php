c2173b31144ffcbd33dcd1ac67ac51f380171d7b
Heru kristanto <herukristanto90@gmail.com>
2019-06-17 02:36:11 +0700

    Update kirim.php

--- kirim.php
+++ kirim.php
@@ -1,20 +1,32 @@
 <?php
-if($_POST['submit']){
-	$admin = 'herukristanto90@gmail.com'; 
-	
-	$nama	= htmlentities($_POST['name']);
-	$email	= htmlentities($_POST['email']);
-	$judul	= htmlentities($_POST['phone']);
-	$pesan	= htmlentities($_POST['massage']);
-	
-	$pengirim	= 'Dari: '.$name.' <'.$email.'>';
-	
-	if(mail($admin, $phone, $massage, $pengirim)){
-		echo 'SUCCESS: Pesan anda berhasil di kirim. <a href="index.php">Kembali</a>';
-	}else{
-		echo 'ERROR: Pesan anda gagal di kirim silahkan coba lagi. <a href="index.php">Kembali</a>';
-	}
-}else{
-	header("Location: index.php");
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$massage=$_POST['massage'];
+
+$to="info@website.com";
+
+$message="Dear, <br /> <br />".$massage;
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+
+// More headers
+$headers .= 'From:'.$name.' <'.$email.'>'."\r\n" . 'Reply-To: '.$name.' <'.$email.'>'."\r\n";
+$headers .= 'Cc: herukristanto90@gmail.com' . "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
+@mail($to,$massage,$massage,$headers);
+if (@mail) { ?>
+<script language="javascript" type="text/javascript">
+alert('Thank you for the message. We will contact you shortly.');
+window.location = 'index.html';
+</script>
+<?php
+}
+else { ?>
+<script language="javascript" type="text/javascript">
+alert('Message failed. Please, send an email to herukristanto90@email.co.id');
+window.location = 'indext.html';
+</script>
+<?php
 }
 ?>
